d0af7d26019b9a50301448c280d66d85dc83f872
Carter Sande <carter.sande@duodecima.technology>
2017-04-05 22:26:16 -0700

    Add support for easily building multiple bottles

---
+++ build-bottles.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+for i in $( cat bottles-to-build.txt ) do
+    brew install --only-dependencies --build-bottle cartr/qt4/$i
+    travis_wait 90 brew install --build-bottle cartr/qt4/$i
+    brew bottle cartr/qt4/$i
+done
